223027f0a44a28636055be02b8400e32785cac16
JingdiC <jingdicxcy@gmail.com>
2021-05-23 19:52:13 -0400

    remove maddpg prefix

--- maddpg/experiments/train_way3_3agents.py
+++ maddpg/experiments/train_way3_3agents.py
@@ -8,10 +8,10 @@ import tensorflow as tf
 import tensorflow.contrib.layers as layers
 import copy
 
-import maddpg.maddpg.common.tf_util as U
-from maddpg.maddpg.trainer.maddpg import MADDPGAgentTrainer, GROUPAgentTrainer, AttentionAgentTrainer
+import maddpg.common.tf_util as U
+from maddpg.trainer.maddpg import MADDPGAgentTrainer, GROUPAgentTrainer, AttentionAgentTrainer
 import matplotlib.pyplot as plt
-from maddpg.experiments.ibmac import IBMACAgentTrainer
+from experiments.ibmac import IBMACAgentTrainer
 
 
 def parse_args():
@@ -19,7 +19,7 @@ def parse_args():
     # Environment
     parser.add_argument("--scenario", type=str, default="simple_spread_way2", help="name of the scenario script")
     parser.add_argument("--max-episode-len", type=int, default=25, help="maximum episode length")
-    parser.add_argument("--num-episodes", type=int, default=600, help="number of episodes")
+    parser.add_argument("--num-episodes", type=int, default=300, help="number of episodes")
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
     parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
@@ -34,17 +34,17 @@ def parse_args():
     parser.add_argument("--ibmac_com", action="store_true", default=True)
 
     # Checkpointing
-    parser.add_argument("--exp-name", type=str, default="jingdi_bw1_spread_3", help="name of the experiment")
+    parser.add_argument("--exp-name", type=str, default="jingdi_bw1_spread_3_bwtest_train1_clip10", help="name of the experiment")
     parser.add_argument("--save-dir", type=str, default="/tmp/policy/", help="directory in which training state and model should be saved")
-    parser.add_argument("--save-rate", type=int, default=4, help="save model once every time this many episodes are completed")
+    parser.add_argument("--save-rate", type=int, default=10, help="save model once every time this many episodes are completed")
     parser.add_argument("--load-dir", type=str, default="", help="directory in which training state and model are loaded")
     # Evaluation
     parser.add_argument("--restore", action="store_true", default=False)
     parser.add_argument("--display", action="store_true", default=False)
     parser.add_argument("--benchmark", action="store_true", default=False)
     parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
-    parser.add_argument("--benchmark-dir", type=str, default="/home/jingdi/JD_MADDPG/maddpg/benchmark_files/", help="directory where benchmark data is saved")
-    parser.add_argument("--plots-dir", type=str, default="/home/jingdi/JD_MADDPG/maddpg/learning_curves/", help="directory where plot data is saved")
+    parser.add_argument("--benchmark-dir", type=str, default="/Users/Chris/JD_MADDPG/maddpg/benchmark_files/", help="directory where benchmark data is saved")
+    parser.add_argument("--plots-dir", type=str, default="/Users/Chris/JD_MADDPG/maddpg/learning_curves/", help="directory where plot data is saved")
     return parser.parse_args()
 
 def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=None):
